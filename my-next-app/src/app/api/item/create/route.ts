dca49a626d461ec9b30988ed6216426e0688b599
Kaba02306 <kabasawa09@gmail.com>
2025-03-01 02:40:32 +0900

    install MongoDB and create POST method

--- my-next-app/src/app/api/item/create/route.ts
+++ my-next-app/src/app/api/item/create/route.ts
@@ -1,6 +1,16 @@
-import { NextResponse } from "next/server"
+import connectDB from "@/app/utils/database"
+import { ItemModel } from "@/app/utils/schemaModels"
+import { NextRequest, NextResponse } from "next/server"
 
-export const POST = async(request: NextResponse) =>{
-    console.log(await request.json())
-    return NextResponse.json({message: "アイテム作成"})
+export const POST = async(request: NextRequest) =>{
+    const reqBody = await request.json()
+    console.log(reqBody)
+    try{
+        await connectDB()
+        await ItemModel.create(reqBody)
+    
+        return NextResponse.json({message: "アイテム作成"})    
+    }catch(err){
+        return NextResponse.json({message: "アイテム作成失敗"})
+    }
 }
---
+++ my-next-app/src/app/utils/database.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const connectDB = async() =>{
+    try{
+        await mongoose.connect("mongodb+srv://kabasawa09:to3R8YfzZmrlB4gr@cluster0.dupmo.mongodb.net/NextMarket15Data?retryWrites=true&w=majority&appName=Cluster0")
+        console.log("Success: Connected to MongoDB")
+    }catch(err){
+        console.log("Failure: Unconnected to MongoDB")
+        throw new Error()
+    }
+}
+
+export default connectDB
